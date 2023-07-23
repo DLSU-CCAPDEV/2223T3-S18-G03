73e6607a08ff53e4b05acb62f0463d62dcb02dc9
Shiray427 <tristan_tan@dlsu.edu.ph>
2023-07-24 00:28:47 +0800

    Update routes.js

--- phase-2/routes/routes.js
+++ phase-2/routes/routes.js
@@ -20,10 +20,13 @@ app.get('/favicon.ico', controller.getFavicon);
     defined in object `controller` in `../controllers/controller.js`
     when a client sends an HTTP GET request for `/`
 */
+
 app.get('/', controller.getIndex);
 
 app.get('/login', controller.redirectLogin);
 app.get('/register', controller.redirectRegister);
 app.get('/post', controller.getExpandedPost);
 app.get('/create', controller.redirectCreatePost);
+app.get('/profile', controller.redirectProfile);
+app.post('/createprofile', controller.createProfile);
 module.exports = app;
